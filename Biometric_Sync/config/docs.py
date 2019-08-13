bf7adb6e529456f11ee9987dd7821272af462375
Sammish Thundiyil <sammish.thundiyil@gmail.com>
2019-08-13 18:17:04 +0300

    new file:   Biometric_Sync/__init__.py 	new file:   Biometric_Sync/config/__init__.py 	new file:   Biometric_Sync/config/desktop.py 	new file:   Biometric_Sync/config/docs.py 	new file:   Biometric_Sync/hooks.py 	new file:   Biometric_Sync/modules.txt 	new file:   Biometric_Sync/patches.txt 	new file:   Biometric_Sync/sync_erp/__init__.py 	new file:   Biometric_Sync/templates/__init__.py 	new file:   Biometric_Sync/templates/pages/__init__.py 	new file:   Biometric_Sync/utils.py

---
+++ Biometric_Sync/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/sammish/Biometric_Sync.git"
+# docs_base_url = "https://github.com/sammish/Biometric_Sync.git"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+	context.brand_html = "Biometric_Sync"
